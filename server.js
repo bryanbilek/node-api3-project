bc897848e4234456bd6db4d992d5664e5ac0f6e7
bryanbilek <bbilek35@gmail.com>
2020-04-09 02:04:07 -0500

    should be working; mvp

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
 const server = express();
+const helmet = require("helmet");
+const userRouter = require("./users/userRouter");
+const postsRouter = require("./posts/postRouter");
+
+//middleware
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+server.use("/api/users", userRouter);
+server.use("/api/posts", postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +18,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl}`);
+  next();
+}
 
 module.exports = server;
